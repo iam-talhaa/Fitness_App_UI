5a59d571d7c63b85b006653076eb2c308349a737
iam-talhaa <tahakhan4141@gmail.com>
2024-08-02 22:40:11 +0500

    Profile Screen

---
+++ lib/ui/Profile_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Profile_screen extends StatefulWidget {
+  const Profile_screen({super.key});
+
+  @override
+  State<Profile_screen> createState() => _Profile_screenState();
+}
+
+class _Profile_screenState extends State<Profile_screen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
