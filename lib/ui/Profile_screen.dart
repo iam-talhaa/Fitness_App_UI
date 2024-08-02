ec03e5c208181b06ff7c3d1bbad2f30a3ac4d58a
iam-talhaa <tahakhan4141@gmail.com>
2024-08-02 23:28:24 +0500

    Profile Screen

--- lib/ui/Profile_screen.dart
+++ lib/ui/Profile_screen.dart
@@ -10,6 +10,53 @@ class Profile_screen extends StatefulWidget {
 class _Profile_screenState extends State<Profile_screen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 31, 28, 28),
+      body: Column(
+        children: [
+          Stack(
+            clipBehavior: Clip.none,
+            fit: StackFit.loose,
+            children: [
+              Image(image: AssetImage("Assets/images/pq5.png")),
+              Positioned(
+                top: 150,
+                left: 100,
+                child: Container(
+                  height: 150,
+                  width: 150,
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black, width: 3),
+                      image: DecorationImage(
+                          fit: BoxFit.fill,
+                          image: AssetImage('Assets/images/p1.png')),
+                      color: Colors.red,
+                      shape: BoxShape.circle),
+                ),
+              ),
+              Positioned(
+                top: 310,
+                left: 30,
+                child: Container(
+                  height: 300,
+                  width: 300,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      color: Colors.black),
+                  child: Column(
+                    children: [
+                      Text(
+                        'Name: TALHA KHAN',
+                        style: TextStyle(fontSize: 20, color: Colors.white),
+                      ),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
